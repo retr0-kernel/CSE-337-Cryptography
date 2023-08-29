c77c6ad451b4e689299a879b13d2cdb9bfd80a2c
Krish Srivastava <krish22092003@gmail.com>
2023-08-29 11:43:03 +0530

    Lab -2

---
+++ Lab 2/q1.cpp
@@ -0,0 +1,27 @@
+/*Write an encryption program such that we get the following input and output.
+Input: computerscienceengineeringsrmuniversity 
+Output: gsqtyxivwgmirgiirkmriivmrkwvqyrmzivwmxc*/
+#include<iostream>
+#include<String>
+
+using namespace std;
+
+string shift_cipher(string str, int shift){
+    int i=0;
+    while(str[i]!='\0'){
+        if((int(str[i])+shift)>122){
+            str[i]=char(int(str[i])+shift-26);
+        }
+        else{
+            str[i]=char(int(str[i])+shift);
+        }
+        i++;
+    }
+    return str;
+}
+int main(){
+    string str="computerscienceengineeringsrmuniversity",cipher;
+    cipher=shift_cipher(str,4);
+    cout<<"The encrypted text is: "+cipher;
+    return 0;
+}
---
+++ Lab 2/q2.cpp
@@ -0,0 +1,33 @@
+/*Raju send an encrypted message (cipher text) “PHHW PH DIWHU WKH WRJD SDUWB” to
+Rani. Can you build decryption process and find out what is the message (plain text) send to
+Rani?*/
+
+#include<iostream>
+#include<String>
+using namespace std;
+string Decrypt(string str, int shift){
+    int i=0;
+    while(str[i]!='\0'){
+        if(str[i]==' ')
+        {
+            i++;
+            continue;
+        }
+        if((int(str[i])-shift)<65){
+            str[i]=char(int(str[i])-shift+26);
+        }
+        else{
+            str[i]=char(int(str[i])-shift);
+        }
+        i++;
+    }
+    return str;
+}
+int main(){
+
+    string str="PHHW PH DIWHU WKH WRJD SDUWB",cipher;
+    cipher=Decrypt(str,3);
+    cout<<"The decrypted text is: "+cipher;
+    return 0;
+
+}
